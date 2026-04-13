8492afc27ac0339b6cc2d5483b537d0f7989f9d3
Micah <micahmartin@gmail.com>
2026-04-12 21:00:00 -0700

    Add archived Clean Code C1 source files

---
+++ archive/CleanCode/C1/src/Farvle.java
@@ -0,0 +1,12 @@
+/**
+ * Farvle -- a class that performs 'Farvle' operations
+ *           on 'Arvadents' and other slithy toves.
+ * @Date: 7 Mar, 2002
+ * @Ship: Dyson
+ * @Guild: Mr. C
+ * @Author: Adelade
+ * Version History:
+ * 20020307 adl -- initial version.
+ */
+public class Farvle {
+}
